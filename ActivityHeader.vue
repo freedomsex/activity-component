2ed8a47a5d8ac101a636be96183bf3d78d41e4c0
tebaly <support@freedomsex.net>
2020-04-01 15:42:08 +0300

    readme

---
+++ ActivityHeader.vue
@@ -0,0 +1,34 @@
+<script>
+import HeaderBar from '@freedomsex/header-bar';
+
+export default {
+  props: ['back', 'caption', 'glass'],
+  computed: {
+    backButton() {
+      return this.back || this.caption;
+    },
+  },
+  components: {
+    HeaderBar,
+  },
+};
+</script>
+
+<template>
+  <HeaderBar :glass="glass" :caption="caption">
+    <div class="header-bar__container">
+      <div class="header-bar__button caption" @click="$emit('close')">
+        <i class="material-icons" v-if="backButton">&#xE5C4;</i>
+        <span class="header-bar__title" >
+          <slot name="caption">
+            {{caption}}
+          </slot>
+        </span>
+      </div>
+
+      <div class="header-bar__navbar">
+        <slot name="option"></slot>
+      </div>
+    </div>
+  </HeaderBar>
+</template>
